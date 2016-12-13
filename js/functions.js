eab966aa8f4048f29c84ecd67fcee932ab7c63c1
mkdeSilva <mihindu.desilva@gmail.com>
2016-12-13 20:59:33 +0700

    Added css, images, and js folders and changed index.html to old website

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+$(window).scroll(function() {
+
+    var scrollDist = $(this).scrollTop();
+
+    $('.minLogo').css({
+        'transform': 'translate(0px, ' + scrollDist / 2 + '%)'
+    });
+
+    $('.back-bird').css({
+        'transform': 'translate(0px, ' + scrollDist / 4 + '%)'
+    });
+
+    $('.fore-bird').css({
+        'transform': 'translate(0px, -' + scrollDist / 50 + '%)'
+    });
+
+    if (scrollDist > 569) {
+        $('nav a').css('color', 'black');
+        $('nav a:hover').css('color', 'purple');
+    } else {
+        $('nav a').css('color', 'white');
+
+    }
+});
