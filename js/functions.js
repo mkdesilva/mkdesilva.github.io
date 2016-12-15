8b99d81f855d18a87bf354ea94b899890f455b39
mkdeSilva <mihindu.desilva@gmail.com>
2016-12-15 15:03:54 +0700

    So done

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,5 @@
-$(window).scroll(function() {
-
-    var scrollDist = $(this).scrollTop();
-
-    console.log(scrollDist);
-    $('.minLogo').css({
-        'transform': 'translate(0px, ' + scrollDist / 1.4 + '%)'
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
     });
-    if (scrollDist > 400) {
-        $('nav').css('background-color', 'rgba(0, 0, 0, 0.3)');
-    } else {
-        $('nav').css('background-color', 'rgba(0, 0, 0, 0)');
-
-    }
 });
