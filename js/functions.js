e22d1bbc87f1d5e734e301939cd3a1440a33aa08
mkdeSilva <mihindu.desilva@gmail.com>
2016-12-15 21:56:24 +0700

    Added additional pictures to website

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,30 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("p").hide();
+$(document).ready(function() {
+    setBindings();
+});
+
+function setBindings() {
+    $(".nav li a").click(function(e) {
+        e.preventDefault();
+        var sectionID = e.currentTarget.id + "Section";
+        var newPosition = $("#" + sectionID).offset().top - 50;
+        $("html body").animate({
+            scrollTop: newPosition
+        }, 600)
     });
+}
+
+$(window).scroll(function() {
+
+    var scrollDist = $(this).scrollTop();
+
+    console.log(scrollDist);
+    $('.minLogo').css({
+        'transform': 'translate(0px, ' + scrollDist / 1.4 + '%)'
+    });
+    if (scrollDist > 400) {
+        $('nav').css('background-color', 'rgba(0, 0, 0, 0.3)');
+    } else {
+        $('nav').css('background-color', 'rgba(0, 0, 0, 0)');
+
+    }
 });
